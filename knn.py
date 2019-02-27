baf2b179a04cd9e2fd2222838348c47a65f77a80
Johan Jobin <26439579+JobinJohan@users.noreply.github.com>
2019-02-27 21:32:21 +0100

    Clean up and PEP8 conventions

--- knn.py
+++ knn.py
@@ -7,64 +7,65 @@ import sys
 import getopt
 
 
-
-
+# Implementation of manhattan distance
 def manhattanDistance(vector1, vector2):
-    return np.sum(np.absolute(vector1-vector2))
+    return np.sum(np.absolute(vector1 - vector2))
+
+# Condensing algorithm that reduces the training set in order to keep only relevant data
+
 
 def condensedTrainingSet(trainingSet, trainLabels, typeOfDistance):
-    newSet=[]
-    newSetLabels=[]
+    newSet = []
+    newSetLabels = []
     newSet.append(trainingSet[0])
     newSetLabels.append(trainLabels[0])
-    
-    trainingSet[1:,:]
+
+    trainingSet[1:, :]
     trainLabels[1:]
-    
+
     for indexLabel1, xi in enumerate(trainingSet):
-        shortestDistance= None
-        labelShortestDistance =""
+        shortestDistance = None
+        labelShortestDistance = ""
         for indexLabel, yi in enumerate(newSet):
-            if typeOfDistance==0:
-                computedDistance = np.linalg.norm(xi-yi)
-            elif typeOfDistance==1:
-                computedDistance = manhattanDistance(xi-yi)
-                
+            # Euclidian distance
+            if typeOfDistance == 0:
+                computedDistance = np.linalg.norm(xi - yi)
+            # Manhattan distance
+            elif typeOfDistance == 1:
+                computedDistance = manhattanDistance(xi - yi)
+
             if shortestDistance == None or computedDistance < shortestDistance:
                 shortestDistance = computedDistance
                 labelShortestDistance = newSetLabels[indexLabel]
-                
+
         if labelShortestDistance != trainLabels[indexLabel1]:
             newSet.append(xi)
             newSetLabels.append(trainLabels[indexLabel1])
     return [newSet, newSetLabels]
 
 
-            
 def getKClosestNeighbors(trainingSet, testSet, trainingLabels, testLabels, k, typeOfDistance):
     correctlyClassified = 0
     for indexTest, testDigit in enumerate(testSet):
         allDistancesFromOneTest = []
         for indexTraining, trainingDigit in enumerate(trainingSet):
-            #euclidian distance
+            # Euclidian distance
             if typeOfDistance == 0:
-                computedDistance = np.linalg.norm(testDigit-trainingDigit)
-            elif typeOfDistance ==1:
+                computedDistance = np.linalg.norm(testDigit - trainingDigit)
+            # Manhattan distance
+            elif typeOfDistance == 1:
                 computedDistance = manhattanDistance(testDigit, trainingDigit)
             allDistancesFromOneTest.append([computedDistance, trainingLabels[indexTraining]])
-        #Sort --> 10 seconds to 1 minute 30 for 1000 and 1000 data
-        #allDistancesFromOneTest = sorted(allDistancesFromOneTest, key=lambda element: element[0]) 
         kClosestNeighbors = [i[1] for i in sorted(allDistancesFromOneTest)[:k]]
         predictedLabel = Counter(kClosestNeighbors).most_common(1)[0][0]
         if(predictedLabel == testLabels[indexTest]):
             correctlyClassified += 1
-            
     print("Accuracy:", (correctlyClassified / len(testSet)) * 100)
     print("End of program")
 
 
-
 def main(argv):
+    # Get arguments from command line
     try:
         opts, args = getopt.getopt(argv, "t:T:k:d:", ["trainingSet=", "TestSet=", "kClosestNeigbors=", "distanceFunction"])
     except getopt.GetoptError:
@@ -79,7 +80,7 @@ def main(argv):
             k = int(arg)
         elif opt in ("-d", "--distanceFunction"):
             d = int(arg)
-    # Main program
+
     # Load data
     trainSet = np.genfromtxt(trainingSet, delimiter=",")
     trainSet = trainSet[:, 1:]
@@ -90,7 +91,6 @@ def main(argv):
     condensedTrainSet = condensedTrainingSet(trainSet, trainLabels, d)
     getKClosestNeighbors(condensedTrainSet[0], testSet, condensedTrainSet[1], testLabels, k, d)
 
-  
-            
+
 if __name__ == "__main__":
     main(sys.argv[1:])
